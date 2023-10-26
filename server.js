869b231e49d30afc524e71abdcb7626e61d695bf
River <riverehill@gmail.com>
2023-10-25 18:12:41 -0700

    Add headers

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ const userRoutes = require('./routes/user')
 app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'POST')
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   next()
 })
 
