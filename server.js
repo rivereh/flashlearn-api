fc91705e4bee5b1ccee2b8d243d0ab12277e8cf4
River <riverehill@gmail.com>
2023-10-27 00:23:39 -0700

    Add crossdomain function

--- server.js
+++ server.js
@@ -9,13 +9,23 @@ const mongoose = require('mongoose')
 const cardRoutes = require('./routes/cards')
 const userRoutes = require('./routes/user')
 
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+app.use(allowCrossDomain)
+
 // middleware
 app.use(express.json())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-  res.setHeader('Access-Control-Allow-Credentials', true)
   console.log(req.path, req.method)
   next()
 })
