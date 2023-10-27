32a0bbe10f45fa1451d0b774e3ca9a9100f3090b
River <riverehill@gmail.com>
2023-10-26 21:38:08 -0700

    Add cors headers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const cors = require('cors')
 
 const app = express()
-app.use(cors())
+// app.use(cors())
 const mongoose = require('mongoose')
 const cardRoutes = require('./routes/cards')
 const userRoutes = require('./routes/user')
@@ -11,6 +11,17 @@ const userRoutes = require('./routes/user')
 // middleware
 app.use(express.json())
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*') //replace localhost with actual host
+  res.header('Access-Control-Allow-Origin', '54.214.110.106') //replace localhost with actual host
+  res.header(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, PUT, PATCH, POST, DELETE'
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, X-Requested-With, Authorization'
+  )
+
   console.log(req.path, req.method)
   next()
 })
