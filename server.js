a92dee93666333d8b2f5e47fd19bd371f9af7e85
River <riverehill@gmail.com>
2023-10-26 21:49:41 -0700

    Update cors

--- server.js
+++ server.js
@@ -11,16 +11,10 @@ const userRoutes = require('./routes/user')
 // middleware
 app.use(express.json())
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') //replace localhost with actual host
-  res.header('Access-Control-Allow-Origin', '54.214.110.106') //replace localhost with actual host
-  res.header(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, PUT, PATCH, POST, DELETE'
-  )
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, X-Requested-With, Authorization'
-  )
+  res.header(`Access-Control-Allow-Origin`, `54.214.110.106`)
+  res.header(`Access-Control-Allow-Origin`, `flashlearnapp.xyz`)
+  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`)
+  res.header(`Access-Control-Allow-Headers`, `Content-Type`)
 
   console.log(req.path, req.method)
   next()
