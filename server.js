b7e758058389bfe0a6f040371a37114a07a07052
River <riverehill@gmail.com>
2023-10-27 19:30:42 -0700

    revert cors changes

--- server.js
+++ server.js
@@ -2,45 +2,29 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 
+const app = express()
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+)
+
 const mongoose = require('mongoose')
 const cardRoutes = require('./routes/cards')
 const userRoutes = require('./routes/user')
 
-const app = express()
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://flashlearn.xyz',
-  'http://flashlearnapp.xyz.s3-website-us-west-2.amazonaws.com',
-]
-
-app.use(cors())
-
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true)
-//       } else {
-//         callback(new Error('Not allowed by CORS'))
-//       }
-//     },
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS', 'DELETE'],
-//     allowedHeaders: '*',
-//   })
-// )
-
+// middleware
+app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
-// middleware
-app.use(express.json())
-
 // routes
-app.use('/api/cards/', cardRoutes)
-app.use('/api/user/', userRoutes)
+app.use('/api/cards', cardRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose
